4de2c06a9d4bd6a93271064e33cbf4e0f7ac9754
Masataka Yakura <masataka.yakura@gmail.com>
2020-07-18 01:03:49 +0900

    feat: grab date from JSON-LD
    grab `datePublished` property from JSON-LD if available

--- heliotropium_content.js
+++ heliotropium_content.js
@@ -2,8 +2,6 @@
 
 function init() {
 	addListeners();
-
-	grabDate();
 }
 
 function addListeners() {
@@ -22,15 +20,48 @@ function handleMessage(message) {
 	chrome.runtime.sendMessage(response);
 }
 
-function grabDate() {
+function grabDateFromTimeElement() {
 	let date = ``;
 	const timeElement = document.querySelector(`time[datetime]`);
 	if (!timeElement) {
 		console.log(`heliotropium: <time> not found.`);
-	}
-	else {
-		console.log(`heliotropium: datetime is "${timeElement.dateTime}"`);
+	} else {
+		console.log(`heliotropium: <time> found.`, timeElement.dateTime);
 		date = timeElement.dateTime;
+	}
+	return date;
+}
+
+function grabDateValueFromJsonLd() {
+	let date = ``;
+	const jsonLdScripts = [
+		...document.querySelectorAll(`script[type="application/ld+json"]`),
+	];
+	const jsonLdDates = jsonLdScripts.filter((script) => {
+		const data = JSON.parse(script.textContent);
+		return `datePublished` in data;
+	});
+	if (!jsonLdDates.length) {
+		console.log(`heliotropium: JSON-LD "datePublished" not found.`);
+	} else {
+		const data = JSON.parse(jsonLdDates[0].textContent);
+		console.log(
+			`heliotropium: JSON-LD "datePublished" found.`,
+			data.datePublished,
+		);
+		date = data.datePublished;
+	}
+	return date;
+}
+
+function grabDate() {
+	let date = ``;
+	if (document.querySelector(`script[type="application/ld+json"]`)) {
+		date = grabDateValueFromJsonLd();
+		return date;
+	} else if (document.querySelector(`time[datetime]`)) {
+		date = grabDateFromTimeElement();
+		return date;
 	}
 	return date;
 }
