8845409b7c0f90b40bbd250d44d63cf4beba03a7
Masataka Yakura <masataka.yakura@gmail.com>
2020-07-23 01:35:04 +0900

    feat: support relative-time element used in GitHub

--- heliotropium_content.js
+++ heliotropium_content.js
@@ -20,6 +20,21 @@ function handleMessage(message) {
 	chrome.runtime.sendMessage(response);
 }
 
+function grabDateFromRelativeTimeElement() {
+	let date = ``;
+	const relativeTimeElement = document.querySelector(`relative-time`);
+	if (!relativeTimeElement) {
+		console.log(`heliotropium: <relative-time> not found.`);
+	} else {
+		console.log(
+			`heliotropium: <relative-time> found.`,
+			relativeTimeElement.getAttribute(`datetime`),
+		);
+		date = relativeTimeElement.getAttribute(`datetime`);
+	}
+	return date;
+}
+
 function grabDateFromTimeElement() {
 	let date = ``;
 	const timeElement = document.querySelector(`time[datetime]`);
@@ -62,6 +77,9 @@ function grabDate() {
 	let date = ``;
 	if (document.querySelector(`script[type="application/ld+json"]`)) {
 		date = grabDateValueFromJsonLd();
+		return date;
+	} else if (document.querySelector(`relative-time`)) {
+		date = grabDateFromRelativeTimeElement();
 		return date;
 	} else if (document.querySelector(`time[datetime]`)) {
 		date = grabDateFromTimeElement();
