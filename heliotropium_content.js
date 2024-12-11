c7d2dabfb93a1ef66289940c5758e88944198a40
Masataka Yakura <masataka.yakura@gmail.com>
2024-12-12 05:00:39 +0900

    refactor: getting value in `getValueFromElement()`

--- heliotropium_content.js
+++ heliotropium_content.js
@@ -75,7 +75,7 @@ function getValueFromElement({ selector, valueAttr = null, scope = document }) {
 	const matched = scope.querySelector(qsArgument);
 	if (!matched) return null;
 
-	const value = (valueAttr ? matched.getAttribute(valueAttr) : matched.textContent).trim();
+	const value = (matched.getAttribute(valueAttr) || matched.textContent || '').trim();
 	console.log(`heliotropium: found ${valueAttr ? `"${valueAttr}" value of ` : ''}"${value}" in`, matched);
 	return value;
 }
