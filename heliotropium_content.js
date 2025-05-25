c0fbb1f7cfdfd68925f10e3d3c167663b75d3d66
Masataka Yakura <masataka.yakura@gmail.com>
2025-05-26 04:11:29 +0900

    feat: add selector for date from SpeakerDeck

--- heliotropium_content.js
+++ heliotropium_content.js
@@ -167,6 +167,7 @@ function findDateFromElementContent() {
 		{ selector: 'p.gargardate' /* Google Search Central Blog */ },
 		{ selector: '.wd-pubdates' /* Chrome Developers, web.dev */ },
 		{ selector: 'devsite-content-footer p:last-child' /* Chrome Developers */ },
+		{ selector: '.deck-date' /* SpeakerDeck */ },
 	];
 	return findValueFromSelectors(contentSelectors);
 }
